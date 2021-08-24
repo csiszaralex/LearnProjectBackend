22a7779b6ca48d34ad986eee9921655192536ccc
Alex <csiszaralex@gmail.com>
2021-08-24 23:28:38 +0200

    :passport_control: Adds guard to file uploads

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -8,11 +8,13 @@ import {
   Delete,
   ValidationPipe,
   Res,
+  UseGuards,
 } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
+import { AuthGuard } from '@nestjs/passport';
 @ApiTags('File')
 @Controller('files')
 export class FilesController {
@@ -25,21 +27,25 @@ export class FilesController {
       limits: { fileSize: 1024 * 1024 * 1024 * 30, fieldSize: 1024 * 1024 * 1024 * 30 },
     }),
   )
+  @UseGuards(AuthGuard())
   uploadFile(@UploadedFile() file: Express.Multer.File): Promise<void> {
     return this.filesService.uploadFile(file);
   }
 
   @Get()
+  @UseGuards(AuthGuard())
   getImages() {
     return this.filesService.getImages();
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard())
   downloadImage(@Param('id', ValidationPipe) id: number, @Res() res: Response) {
     this.filesService.downloadImage(id, res);
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard())
   deleteImage(@Param('id', ValidationPipe) id: number): Promise<void> {
     return this.filesService.deleteImage(id);
   }
